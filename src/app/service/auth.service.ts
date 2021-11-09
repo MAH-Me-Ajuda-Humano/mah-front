aa2d0c06f47ba0c3508ad0caade148bd8ab2cf96
leorusso2300 <leorusso2607@gmail.com>
2021-11-09 15:53:28 -0300

    teste

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,11 +15,11 @@ export class AuthService {
   ) { }
 
   entrar(userLogin : UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('https://projetomah.herokuapp.com/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://mahprojeto.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('https://projetomah.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>('https://mahprojeto.herokuapp.com/usuarios/cadastrar', user)
   }
 
   logado(){
