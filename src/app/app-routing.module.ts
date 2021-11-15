6d5a41e91c6939164f3d90eb9916eaffc01a75b6
leorusso2300 <leorusso2607@gmail.com>
2021-11-15 12:03:50 -0300

    apagar postagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,8 +23,9 @@ const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
   { path: 'tema', component: TemaComponent },
   {path: 'tema-edit/:id', component: TemaEditComponent},
-  {path: 'tema-delete/:id', component: TemaDeleteComponent}
-
+  {path: 'tema-delete/:id', component: TemaDeleteComponent},
+  {path: 'postagem-edit/:id',component: PostagemEditComponent},
+  {path: 'postagem-delete/:id', component: PostagemDeleteComponent}
 ];
 
 @NgModule({
