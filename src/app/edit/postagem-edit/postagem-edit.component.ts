2014a5a6eb9f051caa01f995639942de970ce503
leorusso2300 <leorusso2607@gmail.com>
2021-11-23 14:03:07 -0300

    Deploy

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Postagem } from 'src/app/model/Postagem';
-import { Tema } from 'src/app/model/Tema';
+import { Tema } from 'src/app/model/tema';
 import { PostagemService } from 'src/app/service/postagem.service';
 import { TemaService } from 'src/app/service/tema.service';
 import { environment } from 'src/environments/environment.prod';
